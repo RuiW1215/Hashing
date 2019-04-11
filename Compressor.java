3d255998c54f22be56b5adc23bff00c43676b2a8
RuiW1215 <49537299+RuiW1215@users.noreply.github.com>
2019-04-11 14:22:37 -0700

    Update Compressor.java

--- Compressor.java
+++ Compressor.java
@@ -3,12 +3,44 @@ import java.io.*;
 
 public class Compress {
     public static void main (String [] args) {
+        Scanner input = new Scanner(System.in);
         String filename = args[0];
-        String outputfilename = args[0] + ".zzz";
+        File file = new File(filename);
 
+        // Log file contents
+        int fileSize;
+        int newFileSize;
+        long startTime;
+        long endTime;
+        double hashTableLoad;
+        double avgLinkedListSize;
+        int longestLinkedList;
+        int dicEntries;
+        int rehashes;
+
+
+        boolean validFile = false;
+        while (validFile == false) {
+            if (file.exists() != true){
+                System.out.println("File does not exist. Please input an existing file: ");
+                filename = input.nextLine();
+            }
+            else {
+                validFile = true;
+            }
+        }
+
+        FileInputStream inputS = new FileInputSteam("inputFile.txt");
+        char currentChar;
+        int key = 0;
+        String code = "";
+
+        while ((currentChar = inputS.read()) != -1 ) {
+            System.out.println(currentChar);
+        }
         /* Could convert file into string OR
         *  compress AS the file is being read in
         */
-        HashT dictionary = new HashT[];
+        HashTable dictionary = new HashTable();
     }
 }
